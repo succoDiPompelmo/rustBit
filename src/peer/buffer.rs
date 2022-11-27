d085f4842a12778fb56423cb5391dfea5d00b01b
Giovanni Zenari <zenari12@gmail.com>
2022-11-27 01:21:45 +0100

    Buffer tests

--- src/peer/buffer.rs
+++ src/peer/buffer.rs
@@ -31,7 +31,7 @@ where
     }
 
     pub fn push_message(&mut self, msg: Message) {
-        if (self.message_filter)(&msg) {
+        if (self.message_filter)(&msg) && !self.is_full() {
             self.buffer.push(msg);
         }
     }
@@ -65,5 +65,70 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn test_push_message_not_filterd() {
+        let mut buffer = MessageBuffer::new(|_| true, |_, _| {}, 10);
+        let message = Message::new_raw(vec![], 1, 1);
+
+        buffer.push_message(message);
+        assert_eq!(buffer.buffer.len(), 1);
+        assert!(!buffer.is_full());
+    }
+
+    #[test]
+    fn test_push_message_filterd() {
+        let mut buffer = MessageBuffer::new(|_| false, |_, _| {}, 10);
+        let message = Message::new_raw(vec![], 1, 1);
+
+        buffer.push_message(message);
+        assert_eq!(buffer.buffer.len(), 0);
+        assert!(!buffer.is_full());
+    }
+
+    #[test]
+    fn test_push_message_discarded_when_full() {
+        let mut buffer = MessageBuffer::new(|_| true, |_, _| {}, 0);
+        let message = Message::new_raw(vec![], 1, 1);
+
+        buffer.push_message(message);
+        assert_eq!(buffer.buffer.len(), 0);
+        assert!(buffer.is_full());
+    }
+
+    #[test]
+    fn test_push_message_mix() {
+        let mut buffer = MessageBuffer::new(|msg| msg.get_id() % 2 == 1, |_, _| {}, 2);
+        let message_1 = Message::new_raw(vec![], 1, 1);
+        let message_2 = Message::new_raw(vec![], 1, 2);
+        let message_3 = Message::new_raw(vec![], 1, 3);
+        let message_4 = Message::new_raw(vec![], 1, 5);
+
+        buffer.push_message(message_1);
+        buffer.push_message(message_2);
+        buffer.push_message(message_3);
+        buffer.push_message(message_4);
+        assert_eq!(buffer.buffer.len(), 2);
+        assert!(buffer.is_full());
+    }
+
+    #[test]
+    fn test_assemble_content() {
+        let mut buffer = MessageBuffer::new(|_| true, |_, _| {}, 2);
+        let message_1 = Message::new_raw(vec![0x01], 2, 5);
+        let message_2 = Message::new_raw(vec![0x02, 0x00], 2, 5);
+
+        buffer.push_message(message_1);
+        buffer.push_message(message_2);
+        let expected = vec![0x01, 0x02, 0x00];
+        assert_eq!(buffer.assemble_content(), expected);
+        assert!(buffer.is_full());
+    }
+
+    #[test]
+    fn test_assemble_empty_buffer() {
+        let mut buffer = MessageBuffer::new(|_| true, |_, _| {}, 2);
+
+        let expected: Vec<u8> = vec![];
+        assert_eq!(buffer.assemble_content(), expected);
+        assert!(!buffer.is_full());
+    }
 }
